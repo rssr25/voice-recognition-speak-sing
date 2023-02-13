9faf5baec2f4e4ca9058a40ee286067144663123
rssr25 <rahulsharmacs50@gmail.com>
2023-02-13 18:45:28 +0100

    added utilities

---
+++ src/utilities.py
@@ -0,0 +1,16 @@
+'''
+Created on: Feb 13, 2023 at 18:43
+Created by: Rahul Sharma
+Description: Some utility functions for the project
+'''
+
+
+class Utilities():
+
+    @staticmethod
+    def findCommonData():
+        pass
+    
+    @staticmethod
+    def arrangeData():
+        pass
