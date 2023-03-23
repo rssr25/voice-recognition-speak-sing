fd70e7654a7d9d2f34c97f8eb22167b2e528a5ad
rssr25 <rahulsharmacs50@gmail.com>
2023-03-23 16:37:05 +0100

    created features.py

---
+++ src/features.py
@@ -0,0 +1,15 @@
+'''
+Created on: March 23, 2023 at 16:30
+Created By: Rahul Sharma
+Description: class for extracting different features from the audio files
+'''
+
+class AudioFeatures():
+
+    @staticmethod
+    def lpc(filepath):
+        pass
+    
+    @staticmethod
+    def mfcc(filepath):
+        pass
