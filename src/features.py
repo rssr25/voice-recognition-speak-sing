d62c3772f71df9f9eacd8fbf493f9cadc1320a99
rssr25 <rahulsharmacs50@gmail.com>
2023-03-24 10:39:10 +0100

    finished adding lpc feature extraction

--- src/features.py
+++ src/features.py
@@ -12,9 +12,19 @@ import numpy as np
 class AudioFeatures():
 
     @staticmethod
-    def mfcc(filepath, nmfcc):
+    def mfcc(signal, signalRate, nmfcc):
+        '''
+        This function takes in the signal and the signal rate with the number of the coefficients
+        oen wants and gives out the mfcc. 
+
+        Input: signal (np.ndarray) loaded using librosa,
+               signalRate (float/int) loaded using librosa,
+               nmfcc (int) number of coefficients required
+
+        Output: mfcc coefficients <3
+
+        '''
         #load the file
-        y, sr = librosa.load(filepath)
 
         #calculate log of the spectrogram
         mel_spec = librosa.feature.melspectrogram(y, sr=sr, n_mels=128)
@@ -27,5 +37,22 @@ class AudioFeatures():
 
     
     @staticmethod
-    def lpc(filepath, order, axis):
-        pass
+    def lpc(signal: np.ndarray, signalRate: int, order: int, axis: int) -> np.ndarray:
+        '''
+        This function takes in the signal and the signal rate with the order and axis
+        and number of the coefficients
+        oen wants and gives out the lpc coefficients. 
+
+        Input: signal (np.ndarray) loaded using librosa,
+               signalRate (float/int) loaded using librosa,
+               order (int) rder of the linear filter
+               axis (int) axis along which to compute the coefficients
+
+        Output: lpc coefficients <3
+        
+        '''
+
+        lpc = librosa.lpc(signal, order=order)
+
+        return lpc
+        
