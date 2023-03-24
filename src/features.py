6bf13fa40f3c2fdc09c488c0e2d3204e47372964
rssr25 <rahulsharmacs50@gmail.com>
2023-03-24 10:30:13 +0100

    updated with mfcc code

--- src/features.py
+++ src/features.py
@@ -5,14 +5,25 @@ Description: class for extracting different features from the audio files
 '''
 
 import librosa
-
+import numpy as np
 
 class AudioFeatures():
 
     @staticmethod
-    def lpc(filepath):
-        pass
+    def lpc(filepath, nmfcc):
+        #load the file
+        y, sr = librosa.load(filepath)
+
+        #calculate log of the spectrogram
+        mel_spec = librosa.feature.melspectrogram(y, sr=sr, n_mels=128)
+        log_mel_spec = librosa.logamplitude(mel_spec, ref_power=np.max)
+
+        #get first nmfcc coefficients of the wav file
+        mfcc = librosa.feature.mfcc(S=log_mel_spec, n_mfcc=nmfcc)
+
+        return mfcc
+
     
     @staticmethod
-    def mfcc(filepath):
+    def mfcc(filepath, order, axis):
         pass
