183716f8b6d2ca95d96d684f4b229c3333ce4a40
rssr25 <rahulsharmacs50@gmail.com>
2023-06-06 15:22:15 +0200

    starting code for downloading voxceleb 2

---
+++ src/downloadVoxCelebV2.py
@@ -0,0 +1,16 @@
+'''
+Created by: Rahul Sharma
+Created on: June 6, 2023
+Description: Downloads Voxceleb V2 dataset
+'''
+
+
+
+class DownloadVoxCeleb_V2():
+
+    def __init__(self):
+        pass
+
+
+    def _getDataset(self):
+        pass
