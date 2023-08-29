945e15c527f7cbf133341137720e8bc4ee347093
rssr25 <rahulsharmacs50@gmail.com>
2023-08-29 17:56:11 +0200

    adding multiprocessing to convert voxceleb data

---
+++ src/convert_m4a_to_wav_multiprocessing.py
@@ -0,0 +1,10 @@
+"""
+Created by Rahul on 29.08.2023 at 17:53
+Used to do parallel conversion of files from m4a to wav
+"""
+
+from multiprocessing import Process
+
+
+if __name__ == "__main__":
+    
